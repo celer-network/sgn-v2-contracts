1653e1291722a46bea91bab1f5b8ea7a1be158f9
Michael Zhou <zhoumotongxue008@gmail.com>
2021-10-01 03:05:35 +0800

    Add solt script

---
+++ scripts/solt.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Script to run solt and generate standard-json files for Etherscan verification.
+
+solFiles=(
+	Bridge
+	Staking
+	SGN
+	StakingReward
+	FarmingRewards
+	Govern
+	Viewer
+	interfaces/ISigsVerifier
+)
+
+run_solt_write() {
+	for f in ${solFiles[@]}; do
+		solt write contracts/$f.sol --npm --runs 800
+	done
+}

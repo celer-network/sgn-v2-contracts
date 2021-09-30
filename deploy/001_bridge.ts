34e215f835d56506404e79d4ecb7f317737f56d9
Michael Zhou <zhoumotongxue008@gmail.com>
2021-10-01 04:26:58 +0800

    Add deployment scripts

---
+++ deploy/001_bridge.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+dotenv.config();
+
+const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy('Bridge', {
+    from: deployer,
+    log: true,
+    args: [process.env.BRIDGE_SIGNERS]
+  });
+};
+
+deployFunc.tags = ['Bridge'];
+deployFunc.dependencies = [];
+export default deployFunc;

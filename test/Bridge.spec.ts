98dbd08d3b43255d03857df492ac334bbefa3ef1
Xiaozhou Li <xli@celer.network>
2021-10-04 22:41:29 -0700

    bridge send/relay test

--- test/Bridge.spec.ts
+++ test/Bridge.spec.ts
@@ -1,11 +1,13 @@
 import { expect } from 'chai';
+import { ethers } from 'hardhat';
 
+import { keccak256 } from '@ethersproject/solidity';
 import { parseUnits } from '@ethersproject/units';
 import { Wallet } from '@ethersproject/wallet';
 
 import { Bridge, TestERC20 } from '../typechain';
 import { deployBridgeContracts, getAccounts, loadFixture } from './lib/common';
-import { getSignersBytes, getUpdateSignersRequest } from './lib/proto';
+import { getSignersBytes, getUpdateSignersRequest, getRelayRequest } from './lib/proto';
 
 describe('Bridge Tests', function () {
   async function fixture([admin]: Wallet[]) {
@@ -81,5 +83,53 @@ describe('Bridge Tests', function () {
     )
       .to.emit(bridge, 'SignersUpdated')
       .withArgs('0x' + Buffer.from(req.newSignersBytes).toString('hex'));
+  });
+
+  it('should send and successfully', async function () {
+    const signers = [accounts[0], accounts[1], accounts[2]];
+    const signersAddrs = [accounts[0].address, accounts[1].address, accounts[2].address];
+    const powers = [parseUnits('10'), parseUnits('10'), parseUnits('10')];
+    const initSignersBytes = await getSignersBytes(signersAddrs, powers, true);
+    await expect(bridge.setInitSigners(initSignersBytes))
+      .to.emit(bridge, 'SignersUpdated')
+      .withArgs('0x' + Buffer.from(initSignersBytes).toString('hex'));
+
+    const sender = accounts[0];
+    const receiver = accounts[1];
+    const amount = parseUnits('1');
+    const chainId = (await ethers.provider.getNetwork()).chainId;
+    const nonce = 0;
+    const slippage = 1000;
+
+    const srcXferId = keccak256(
+      ['address', 'address', 'address', 'uint256', 'uint64', 'uint64', 'uint64'],
+      [sender.address, receiver.address, token.address, amount, chainId, nonce, chainId]
+    );
+
+    await token.connect(sender).approve(bridge.address, parseUnits('100'));
+    await expect(bridge.connect(sender).send(receiver.address, token.address, amount, chainId, nonce, slippage))
+      .to.emit(bridge, 'Send')
+      .withArgs(srcXferId, sender.address, receiver.address, token.address, amount, chainId, nonce, slippage);
+
+    const { relayBytes, curss, sigs } = await getRelayRequest(
+      sender.address,
+      receiver.address,
+      token.address,
+      amount,
+      chainId,
+      chainId,
+      srcXferId,
+      signers,
+      powers
+    );
+
+    const dstXferId = keccak256(
+      ['address', 'address', 'address', 'uint256', 'uint64', 'uint64', 'bytes32'],
+      [sender.address, receiver.address, token.address, amount, chainId, chainId, srcXferId]
+    );
+
+    await expect(bridge.relay(relayBytes, curss, sigs))
+      .to.emit(bridge, 'Relay')
+      .withArgs(dstXferId, sender.address, receiver.address, token.address, amount, chainId, srcXferId);
   });
 });

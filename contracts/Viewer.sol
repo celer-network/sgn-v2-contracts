3120f509815a1b15e406939f51f2f24e728b0c8a
Xiaozhou Li <xli@celer.network>
2021-10-05 12:46:54 -0700

    solc v0.8.9

--- contracts/Viewer.sol
+++ contracts/Viewer.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-pragma solidity 0.8.7;
+pragma solidity 0.8.9;
 
 import {DataTypes as dt} from "./libraries/DataTypes.sol";
 import "./Staking.sol";
--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-pragma solidity 0.8.7;
+pragma solidity 0.8.9;
 
 abstract contract Whitelist {
     mapping(address => bool) public whitelist;

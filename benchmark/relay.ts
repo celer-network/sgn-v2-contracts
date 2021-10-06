662d4aa6055f12d0b4426e57c8cbb5bf71b5e6a1
Xiaozhou Li <xli@celer.network>
2021-10-06 01:23:57 -0700

    update benchmark

--- benchmark/relay.ts
+++ benchmark/relay.ts
@@ -22,7 +22,7 @@ describe('Relay Gas Benchmark', function () {
     fs.mkdirSync(GAS_USAGE_DIR, { recursive: true });
   }
   fs.rmSync(GAS_USAGE_LOG, { force: true });
-  fs.appendFileSync(GAS_USAGE_LOG, '<signer num, quorum sig num, gas cost> for cbr relay tx\n\n');
+  fs.appendFileSync(GAS_USAGE_LOG, '<validatorNum, quorumSigNum, gasCost> for cbr testErc20 relay tx\n\n');
 
   async function fixture([admin]: Wallet[]) {
     const { bridge, token } = await deployBridgeContracts(admin);
@@ -44,71 +44,92 @@ describe('Relay Gas Benchmark', function () {
   });
 
   it('benchmark relay gas cost for bridge', async function () {
-    await doBenchmarkBridge(4, 12, 3);
-    await doBenchmarkBridge(5, 15, 6);
-    await doBenchmarkBridge(10, 21, 10);
-    await doBenchmarkBridge(15, 21, 15);
+    let perSigCost;
+    for (let i = 5; i <= 21; i += 2) {
+      perSigCost = await doBenchmarkRelaySig(i);
+    }
+    fs.appendFileSync(GAS_USAGE_LOG, 'per sig cost: ' + perSigCost + '\n');
+    fs.appendFileSync(GAS_USAGE_LOG, '\n');
+
+    const perSignerCost = await doBenchmarkRelaySigner(21, 8);
+    fs.appendFileSync(GAS_USAGE_LOG, 'per validator cost: ' + perSignerCost + '\n');
   });
 
   async function getPowers(
     accounts: Wallet[],
-    num: number,
-    quorumSigs: number
-  ): Promise<{ signers: Wallet[]; addrs: string[]; powers: BigNumber[]; quorumSigNum: number }> {
-    const maxQuorumSigs = (num * 2) / 3 + 1;
-    if (quorumSigs > maxQuorumSigs) {
-      quorumSigs = maxQuorumSigs | 0;
-    }
+    signerNum: number,
+    quorumSigNum: number
+  ): Promise<{ signers: Wallet[]; addrs: string[]; powers: BigNumber[] }> {
     const signers: Wallet[] = [];
     const addrs: string[] = [];
     const powers: BigNumber[] = [];
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < signerNum; i++) {
       signers.push(accounts[i]);
       addrs.push(accounts[i].address);
-      if (i == quorumSigs - 1) {
+      if (i == quorumSigNum - 1) {
         powers.push(parseUnits('100'));
       } else {
         powers.push(parseUnits('1'));
       }
     }
-    const quorumSigNum = quorumSigs;
-    return { signers, addrs, powers, quorumSigNum };
+    return { signers, addrs, powers };
   }
 
-  async function doBenchmarkBridge(minNum: number, maxNum: number, quorumSigs: number) {
-    fs.appendFileSync(GAS_USAGE_LOG, 'max number of sigs to reach quorum: ' + quorumSigs + '\n');
+  async function doBenchmarkRelaySig(signerNum: number) {
     let firstCost = 0;
     let lastCost = 0;
-    for (let i = minNum; i <= maxNum; i++) {
-      const { signers, addrs, powers, quorumSigNum } = await getPowers(accounts, i, quorumSigs);
-      await bridge.notifyResetSigners();
-      await bridge.resetSigners(addrs, powers);
-
-      const sender = accounts[0];
-      const receiver = accounts[1];
-      const amount = parseUnits('1');
-      const chainId = (await ethers.provider.getNetwork()).chainId;
-      const srcXferId = keccak256(['uint64'], [Date.now()]); // fake src xfer id
-      const { relayBytes, sigs } = await getRelayRequest(
-        sender.address,
-        receiver.address,
-        token.address,
-        amount,
-        chainId,
-        chainId,
-        srcXferId,
-        signers,
-      );
-      const gasUsed = (await (await bridge.relay(relayBytes, sigs, addrs, powers)).wait()).gasUsed;
-      if (i == minNum) {
+    const maxQuorumSigNum = ((signerNum * 2) / 3 + 1) | 0;
+    for (let i = 3; i <= maxQuorumSigNum; i += 2) {
+      const gasUsed = await doBenchmarkRelay(signerNum, i);
+      if (i == 3) {
         firstCost = gasUsed.toNumber();
       }
-      if (i == maxNum) {
-        lastCost = gasUsed.toNumber();
-      }
-      fs.appendFileSync(GAS_USAGE_LOG, i.toString() + '\t' + quorumSigNum.toString() + '\t' + gasUsed + '\n');
+      lastCost = gasUsed.toNumber();
     }
-    const perSigCost = Math.ceil((lastCost - firstCost) / (maxNum - minNum));
-    fs.appendFileSync(GAS_USAGE_LOG, 'per signer cost: ' + perSigCost + '\n\n');
+    const perSigCost = Math.ceil((lastCost - firstCost) / (maxQuorumSigNum - 3));
+    return perSigCost;
+  }
+
+  async function doBenchmarkRelaySigner(maxSignerNum: number, quorumSigNum: number) {
+    let firstCost = 0;
+    let lastCost = 0;
+    const minSignerNum = ((quorumSigNum * 3) / 2) | 0;
+    for (let i = minSignerNum; i <= maxSignerNum; i++) {
+      const gasUsed = await doBenchmarkRelay(i, quorumSigNum);
+      if (i == minSignerNum) {
+        firstCost = gasUsed.toNumber();
+      }
+      lastCost = gasUsed.toNumber();
+    }
+    const perSignerCost = Math.ceil((lastCost - firstCost) / (maxSignerNum - minSignerNum));
+    return perSignerCost;
+  }
+
+  async function doBenchmarkRelay(signerNum: number, quorumSigNum: number) {
+    if (quorumSigNum > signerNum) {
+      quorumSigNum = signerNum;
+    }
+    const { signers, addrs, powers } = await getPowers(accounts, signerNum, quorumSigNum);
+    await bridge.notifyResetSigners();
+    await bridge.resetSigners(addrs, powers);
+
+    const sender = accounts[0];
+    const receiver = accounts[1];
+    const amount = parseUnits('1');
+    const chainId = (await ethers.provider.getNetwork()).chainId;
+    const srcXferId = keccak256(['uint64'], [Date.now()]); // fake src xfer id
+    const { relayBytes, sigs } = await getRelayRequest(
+      sender.address,
+      receiver.address,
+      token.address,
+      amount,
+      chainId,
+      chainId,
+      srcXferId,
+      signers
+    );
+    const gasUsed = (await (await bridge.relay(relayBytes, sigs, addrs, powers)).wait()).gasUsed;
+    fs.appendFileSync(GAS_USAGE_LOG, signerNum.toString() + '\t' + quorumSigNum.toString() + '\t' + gasUsed + '\n');
+    return gasUsed;
   }
 });

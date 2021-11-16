466e2bfe83365b02c521af9d02f550fc3ce4a932
Michael Zhou <zhoumotongxue008@gmail.com>
2021-11-16 16:18:58 +0800

    Update mainnet deployment template

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,9 @@ import '@typechain/hardhat';
 import 'hardhat-contract-sizer';
 import 'hardhat-deploy';
 import 'hardhat-gas-reporter';
-import { HardhatUserConfig } from 'hardhat/types';
+
 import * as dotenv from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/types';
 
 dotenv.config();
 
@@ -27,8 +28,23 @@ const bscTestPrivateKey = process.env.BSC_TEST_PRIVATE_KEY || DEFAULT_PRIVATE_KE
 const fantomTestEndpoint = process.env.FANTOM_TEST_ENDPOINT || DEFAULT_ENDPOINT;
 const fantomTestPrivateKey = process.env.FANTOM_TEST_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
 
-const mainnetEndpoint = process.env.MAINNET_ENDPOINT || DEFAULT_ENDPOINT;
-const mainnetPrivateKey = process.env.MAINNET_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
+const ethMainnetEndpoint = process.env.ETH_MAINNET_ENDPOINT || DEFAULT_ENDPOINT;
+const ethMainnetPrivateKey = process.env.ETH_MAINNET_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
+
+const bscEndpoint = process.env.BSC_ENDPOINT || DEFAULT_ENDPOINT;
+const bscPrivateKey = process.env.BSC_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
+
+const arbitrumEndpoint = process.env.ARBITRUM_ENDPOINT || DEFAULT_ENDPOINT;
+const arbitrumPrivateKey = process.env.ARBITRUM_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
+
+const polygonEndpoint = process.env.POLYGON_ENDPOINT || DEFAULT_ENDPOINT;
+const polygonPrivateKey = process.env.POLYGON_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
+
+const fantomEndpoint = process.env.FANTOM_ENDPOINT || DEFAULT_ENDPOINT;
+const fantomPrivateKey = process.env.FANTOM_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
+
+const avalancheEndpoint = process.env.AVALANCHE_ENDPOINT || DEFAULT_ENDPOINT;
+const avalanchePrivateKey = process.env.AVALANCHE_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
@@ -55,9 +71,29 @@ const config: HardhatUserConfig = {
       url: fantomTestEndpoint,
       accounts: [`0x${fantomTestPrivateKey}`]
     },
-    mainnet: {
-      url: mainnetEndpoint,
-      accounts: [`0x${mainnetPrivateKey}`]
+    ethMainnet: {
+      url: ethMainnetEndpoint,
+      accounts: [`0x${ethMainnetPrivateKey}`]
+    },
+    bsc: {
+      url: bscEndpoint,
+      accounts: [`0x${bscPrivateKey}`]
+    },
+    arbitrum: {
+      url: arbitrumEndpoint,
+      accounts: [`0x${arbitrumPrivateKey}`]
+    },
+    polygon: {
+      url: polygonEndpoint,
+      accounts: [`0x${polygonPrivateKey}`]
+    },
+    fantom: {
+      url: fantomEndpoint,
+      accounts: [`0x${fantomPrivateKey}`]
+    },
+    avalanche: {
+      url: avalancheEndpoint,
+      accounts: [`0x${avalanchePrivateKey}`]
     }
   },
   namedAccounts: {

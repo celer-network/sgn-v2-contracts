efa0c5548010fd18f54595e500a4536e22a0d8ca
Xiaozhou Li <xli@celer.network>
2021-12-19 23:10:21 -0800

    address comments

--- contracts/pegged/tokens/MintSwapCanonicalToken.sol
+++ contracts/pegged/tokens/MintSwapCanonicalToken.sol
@@ -2,14 +2,17 @@
 
 pragma solidity >=0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./MultiBridgeToken.sol";
 
 /**
  * @title Canonical token that supports multi-bridge minter and multi-token swap
  */
 contract MintSwapCanonicalToken is MultiBridgeToken {
-    // each bridge token.balanceOf(this) tracks how much that bridge has already swapped
-    mapping(address => uint256) public tokenSwapCap; // each bridge token -> swap cap
+    using SafeERC20 for IERC20;
+
+    // bridge token -> cap of total swapped amount, can be tracked by each bridge token.balanceOf(this)
+    mapping(address => uint256) public totalSwapCap;
 
     event TokenSwapCapUpdated(address token, uint256 cap);
 
@@ -20,23 +23,25 @@ contract MintSwapCanonicalToken is MultiBridgeToken {
     ) MultiBridgeToken(name_, symbol_, decimals_) {}
 
     // msg.sender has bridge token and want to get canonical token
-    function swapBridgeForCanonical(address _bridgeToken, uint256 _amount) external {
+    function swapBridgeForCanonical(address _bridgeToken, uint256 _amount) external returns (uint256) {
         // move bridge token from msg.sender to canonical token _amount
-        IERC20(_bridgeToken).transferFrom(msg.sender, address(this), _amount);
-        require(IERC20(_bridgeToken).balanceOf(address(this)) < tokenSwapCap[_bridgeToken], "exceed swap cap");
+        IERC20(_bridgeToken).safeTransferFrom(msg.sender, address(this), _amount);
+        require(IERC20(_bridgeToken).balanceOf(address(this)) < totalSwapCap[_bridgeToken], "exceed swap cap");
         _mint(msg.sender, _amount);
+        return _amount;
     }
 
     // msg.sender has canonical and want to get bridge token (eg. for cross chain burn)
-    function swapCanonicalForBridge(address _bridgeToken, uint256 _amount) external {
+    function swapCanonicalForBridge(address _bridgeToken, uint256 _amount) external returns (uint256) {
         _burn(msg.sender, _amount);
-        IERC20(_bridgeToken).transfer(msg.sender, _amount);
+        IERC20(_bridgeToken).safeTransfer(msg.sender, _amount);
+        return _amount;
     }
 
     // update existing bridge token swap cap or add a new bridge token with swap cap
     // set cap to 0 will disable swapBridgeForCanonical, but swapCanonicalToBridge will still work
     function setBridgeTokenSwapCap(address _bridgeToken, uint256 _swapCap) external onlyOwner {
-        tokenSwapCap[_bridgeToken] = _swapCap;
+        totalSwapCap[_bridgeToken] = _swapCap;
         emit TokenSwapCapUpdated(_bridgeToken, _swapCap);
     }
 }

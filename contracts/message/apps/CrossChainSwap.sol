80a769b09a9b7f5c7a541a8afd95cd10b8c46288
Junda Liu <junda@celer.network>
2021-12-21 10:10:26 -0800

    fix

--- contracts/message/apps/CrossChainSwap.sol
+++ contracts/message/apps/CrossChainSwap.sol
@@ -50,7 +50,11 @@ contract CrossChainSwap is MsgSenderApp, MsgReceiverApp {
         sendMessageWithTransfer(_receiver, _token, _amount, _dstChainId, nonce, swapInfo.cbrMaxSlippage, message);
     }
 
-    // handleMessage?
+    function handleMessage(
+        address _sender,
+        uint64 _srcChainId,
+        bytes memory _message
+    ) internal override {}
 
 
     // ========== on swap chain ==========
@@ -64,14 +68,15 @@ contract CrossChainSwap is MsgSenderApp, MsgReceiverApp {
     ) internal override {
         SwapInfo memory swapInfo = abi.decode((_message), (SwapInfo));
         IERC20(_token).approve(dex, _amount);
+        address[] memory path = [_token, swapInfo.wantToken];
         if (swapInfo.sendBack) {
             nonce += 1;
-            uint256[] memory swapReturn = ISwapToken(dex).swapExactTokensForTokens(_amount, 0, [_token, swapInfo.wantToken], address(this), type(uint256).max);
+            uint256[] memory swapReturn = ISwapToken(dex).swapExactTokensForTokens(_amount, 0, path, address(this), type(uint256).max);
             // send received token back to start chain. swapReturn[1] is amount of wantToken
             IBridge(liquidityBridge).send(swapInfo.user, swapInfo.wantToken, swapReturn[1], _srcChainId, nonce, swapInfo.cbrMaxSlippage);
         } else {
             // swap to wantToken and send to user
-            ISwapToken(dex).swapExactTokensForTokens(_amount, 0, [_token, swapInfo.wantToken], swapInfo.user, type(uint256).max);
+            ISwapToken(dex).swapExactTokensForTokens(_amount, 0, path, swapInfo.user, type(uint256).max);
         }
         // bytes memory notice; // send back to src chain to handleMessage
         // sendMessage(_sender, _srcChainId, notice);

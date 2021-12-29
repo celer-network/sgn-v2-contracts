082444477d78fe77b68c9846e8c875703eeded37
patrickmao93 <patrickmao93@gmail.com>
2021-12-29 14:45:48 +0800

    fix

--- contracts/message/apps/TransferSwap.sol
+++ contracts/message/apps/TransferSwap.sol
@@ -16,8 +16,12 @@ contract TransferSwap is MsgSenderApp, MsgReceiverApp {
         uint256 minRecvAmt;
     }
 
+    struct SwapRequest {
+        Swap swap;
+        address receiver;
+    }
+
     uint64 SlippageDenominator = 1e6;
-    uint64 nonce; // required by IBridge.send
 
     function transferWithSwap(
         address _receiver,
@@ -25,17 +29,18 @@ contract TransferSwap is MsgSenderApp, MsgReceiverApp {
         uint64 _dstChainId,
         Swap calldata _srcSwap,
         Swap calldata _dstSwap,
-        uint32 _maxBridgeSlippage
+        uint32 _maxBridgeSlippage,
+        uint64 _nonce
     ) external {
-        address intermediaryToken = _srcSwap.path[_srcSwap.path.length - 1];
+        address bridgeToken = _srcSwap.path[_srcSwap.path.length - 1];
+        uint256 bridgeTokenAmt = _amountIn;
+
         require(_srcSwap.path.length > 0, "empty src swap path");
         require(_dstSwap.path.length > 0, "empty dst swap path");
         require(
-            intermediaryToken == _dstSwap.path[0],
+            bridgeToken == _dstSwap.path[0],
             "the last token in _srcSwapPath and the first token in dstPath must be the same"
         );
-
-        nonce += 1;
 
         // pull source token from user
         IERC20(_srcSwap.path[0]).safeTransferFrom(msg.sender, address(this), _amountIn);
@@ -43,24 +48,26 @@ contract TransferSwap is MsgSenderApp, MsgReceiverApp {
         // swap original token for intermediate token on the source DEX
         if (_srcSwap.path.length > 1) {
             IERC20(_srcSwap.path[0]).approve(_srcSwap.dex, _amountIn);
-            IUniswapV2(_srcSwap.dex).swapExactTokensForTokens(
+            uint256[] memory amounts = IUniswapV2(_srcSwap.dex).swapExactTokensForTokens(
                 _amountIn,
                 _srcSwap.minRecvAmt,
                 _srcSwap.path,
                 address(this),
                 _srcSwap.deadline
             );
+            bridgeTokenAmt = amounts[amounts.length - 1];
         }
 
         // bridge the intermediate token to destination chain with message
+        SwapRequest memory message = SwapRequest({swap: _dstSwap, receiver: _receiver});
         sendMessageWithTransfer(
             _receiver,
-            intermediaryToken,
-            IERC20(intermediaryToken).balanceOf(msg.sender),
+            bridgeToken,
+            bridgeTokenAmt,
             _dstChainId,
-            nonce,
+            _nonce,
             _maxBridgeSlippage,
-            abi.encode(_dstSwap)
+            abi.encode(message)
         );
     }
 
@@ -71,13 +78,22 @@ contract TransferSwap is MsgSenderApp, MsgReceiverApp {
         uint64, // _srcChainId
         bytes memory _message
     ) external override onlyMessageBus {
-        Swap memory m = abi.decode((_message), (Swap));
-        require(_token == m.path[0], "bridged token must be the same as the first token in destination swap path");
+        SwapRequest memory m = abi.decode((_message), (SwapRequest));
+        require(_token == m.swap.path[0], "bridged token must be the same as the first token in destination swap path");
 
-        // swap intermediate token to the token user wants on the destination DEX
-        if (m.path.length > 1) {
-            IERC20(m.path[0]).approve(m.dex, _amount);
-            IUniswapV2(m.dex).swapExactTokensForTokens(_amount, m.minRecvAmt, m.path, address(this), m.deadline);
+        if (m.swap.path.length > 1) {
+            // swap intermediate token to the token user wants on the destination DEX
+            IERC20(m.swap.path[0]).approve(m.swap.dex, _amount);
+            IUniswapV2(m.swap.dex).swapExactTokensForTokens(
+                _amount,
+                m.swap.minRecvAmt,
+                m.swap.path,
+                m.receiver,
+                m.swap.deadline
+            );
+        } else {
+            // no need to swap, directly send the bridged token to user
+            IERC20(_token).transfer(m.receiver, _amount);
         }
     }
 }

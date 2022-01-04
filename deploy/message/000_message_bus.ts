33bd6a3fc8a916d114171b6e7f1d880f3f3b5548
patrickmao93 <patrickmao93@gmail.com>
2022-01-04 17:10:44 +0800

    add deploy scripts

---
+++ deploy/message/000_message_bus.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+dotenv.config();
+
+const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy('MessageBus', {
+    from: deployer,
+    log: true
+  });
+};
+
+deployFunc.tags = ['MessageBus'];
+deployFunc.dependencies = [];
+export default deployFunc;
---
+++ deploy/message/001_transfer_swap.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+dotenv.config();
+
+const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy('TransferSwap', {
+    from: deployer,
+    log: true
+  });
+};
+
+deployFunc.tags = ['TransferSwap'];
+deployFunc.dependencies = [];
+export default deployFunc;

255d330132b3fd3f53a6edf462ac7724879a0ffc
Elliott He <elliott.he@outlook.com>
2022-01-10 17:04:43 +0800

    deploy scripts update

--- deploy/message/001_transfer_swap.ts
+++ deploy/message/001_transfer_swap.ts
@@ -12,7 +12,7 @@ const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   await deploy('TransferSwap', {
     from: deployer,
     log: true,
-    args: [process.env.CC_SWAP_MSG_BUS]
+    args: [process.env.CC_SWAP_MSG_BUS, process.env.CC_SWAP_DEX, process.env.CC_SWAP_BRIDGE, process.env.CC_SWAP_BRIDGE_TOKEN]
   });
 };
 

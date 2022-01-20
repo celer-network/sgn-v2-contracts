9c72b54eefcaf097c66355dadcb479887309ef66
Sanbao Zheng <baobaobao369@163.com>
2022-01-20 17:28:20 +0800

    Add WithdrawInbox.sol (#112)
    * Add WithdrawInbox.sol
    
    * Fix calldata error
    
    * Refine typo and add necessary check
    
    * Refine length checks
    
    * npx prettier -w contracts/WithdrawInbox.sol

---
+++ contracts/WithdrawInbox.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity 0.8.9;
+
+contract WithdrawInbox {
+    // contract LP withdrawal request
+    event WithdrawalRequest(
+        address sender,
+        address receiver,
+        uint64 toChain,
+        uint64[] fromChains,
+        address[] tokens,
+        uint32[] ratios,
+        uint32[] slippages
+    );
+
+    /**
+     * @notice Withdraw liquidity from the pool-based bridge.
+     * @param _receiver The receiver address on _toChain.
+     * @param _toChain The chain Id to receive the withdrawn tokens.
+     * @param _fromChains The chain Ids to withdraw tokens.
+     * @param _tokens The token to withdraw on each fromChain.
+     * @param _ratios The withdrawal ratios of each token.
+     * @param _slippages The max slippages of each token for cross-chain withdraw.
+     */
+    function withdraw(
+        address _receiver,
+        uint64 _toChain,
+        uint64[] calldata _fromChains,
+        address[] calldata _tokens,
+        uint32[] calldata _ratios,
+        uint32[] calldata _slippages
+    ) external {
+        require(
+            _tokens.length == _fromChains.length &&
+                _ratios.length == _fromChains.length &&
+                _slippages.length == _fromChains.length,
+            "length mismatch"
+        );
+        emit WithdrawalRequest(msg.sender, _receiver, _toChain, _fromChains, _tokens, _ratios, _slippages);
+    }
+}

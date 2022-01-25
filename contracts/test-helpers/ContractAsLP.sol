9702b3a401ea502d7f646dffb9c343cde4b73896
ClaudeZsb <baobaobao369@163.com>
2022-01-25 16:14:36 +0800

    move example contract to test-helpers

--- contracts/test-helpers/ContractAsLP.sol
+++ contracts/test-helpers/ContractAsLP.sol
@@ -5,10 +5,9 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./interfaces/IPool.sol";
-import "./interfaces/IWithdrawInbox.sol";
-import "./safeguard/Ownable.sol";
-import "./safeguard/Pauser.sol";
+import "IPool.sol";
+import "IWithdrawInbox.sol";
+import "../safeguard/Pauser.sol";
 
 contract ContractAsLP is ReentrancyGuard, Pauser {
     using SafeERC20 for IERC20;

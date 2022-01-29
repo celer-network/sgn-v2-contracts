cf8ce3bd69de268306c30ab4e30694acc4a12ffc
ClaudeZsb <baobaobao369@163.com>
2022-01-29 16:05:35 +0800

    replace contract interface

--- contracts/test-helpers/ContractAsLP.sol
+++ contracts/test-helpers/ContractAsLP.sol
@@ -5,8 +5,8 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./IPool.sol";
-import "./IWithdrawInbox.sol";
+import "../interfaces/IPool.sol";
+import "../interfaces/IWithdrawInbox.sol";
 import "../safeguard/Pauser.sol";
 
 contract ContractAsLP is ReentrancyGuard, Pauser {

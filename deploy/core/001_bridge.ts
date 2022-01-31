fa98969d594322c572f3143e6aa55ef994242558
Gregory Luneau <gregory.luneau@gmail.com>
2022-01-31 17:06:42 -0500

    calcEthereumTransactionParams

--- deploy/core/001_bridge.ts
+++ deploy/core/001_bridge.ts
@@ -1,7 +1,8 @@
 import * as dotenv from 'dotenv';
-import { BigNumber } from 'ethers';
+import { ethers } from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { calcEthereumTransactionParams } from '@acala-network/eth-providers';
 
 dotenv.config();
 
@@ -9,12 +10,21 @@ const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+  const blockNumber = await ethers.provider.getBlockNumber();
+
+  const ethParams = calcEthereumTransactionParams({
+    gasLimit: '2100001',
+    validUntil: (blockNumber + 100).toString(),
+    storageLimit: '64001',
+    txFeePerGas,
+    storageByteDeposit
+  });
 
   await deploy('Bridge', {
     from: deployer,
     log: true,
-    gasLimit: BigNumber.from('42032000'), // Mandala
-    gasPrice: BigNumber.from('200786445289'), // Mandala
+    gasLimit: ethParams.txGasLimit, // Mandala
+    gasPrice: ethParams.txGasPrice, // Mandala
   });
 };
 

646d482262a6e156e0f76f9a27b4bd978b3ec362
Gregory Luneau <gregory.luneau@gmail.com>
2022-02-01 13:31:28 -0500

    storageByteDeposit, txFeePerGas

--- deploy/core/001_bridge.ts
+++ deploy/core/001_bridge.ts
@@ -3,6 +3,7 @@ import { ethers } from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { calcEthereumTransactionParams } from '@acala-network/eth-providers';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 dotenv.config();
 
@@ -11,13 +12,18 @@ const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
   const blockNumber = await ethers.provider.getBlockNumber();
+  const wsProvider = new WsProvider('wss://node-6870830370282213376.rz.onfinality.io/ws?apikey=0f273197-e4d5-45e2-b23e-03b015cb7000');
+  const api = await ApiPromise.create({ provider: wsProvider });
+  const storageByteDeposit = (api.consts.evm.storageDepositPerByte).toString();
+  const txFeePerGas = (api.consts.evm.txFeePerGas).toString();
 
+  console.log(storageByteDeposit, txFeePerGas)
   const ethParams = calcEthereumTransactionParams({
-    gasLimit: '2100001',
+    gasLimit: '21000000',
     validUntil: (blockNumber + 100).toString(),
     storageLimit: '64001',
     txFeePerGas,
-    storageByteDeposit
+    storageByteDeposit,
   });
 
   await deploy('Bridge', {

63f81e0930f4cb3a1e2a121291ae2acd2e60c94f
Junda Liu <junda@celer.network>
2022-03-14 17:32:44 -0700

    add NFT Bridge and NFToken to work with it

---
+++ contracts/message/apps/NFToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage {
+    address public nftBridge;
+    constructor(string memory name_, string memory symbol_, address _nftBridge) ERC721(name_, symbol_) {
+        nftBridge = _nftBridge;
+    }
+    modifier onlyNftBridge() {
+        require(msg.sender == nftBridge, "caller is not bridge");
+        _;
+    }
+
+    function mint(address to, uint256 id, string memory uri) external onlyNftBridge {
+        _mint(to, id);
+        _setTokenURI(id, uri);
+    }
+
+    function burn(uint256 id) external onlyNftBridge {
+        _burn(id);
+    }
+}

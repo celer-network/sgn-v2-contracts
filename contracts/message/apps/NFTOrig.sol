c09118d076632f60f502ce5758c7d45544041c77
i9 <i9@users.noreply.github.com>
2022-03-21 13:36:38 -0700

    NFT Bridge (#134)
    * add NFT Bridge and NFToken to work with it
    * add receiver arg and make nftBridge immutable
    * rename NFToken to NFTPeg, add NFTOrig and deploy scripts

---
+++ contracts/message/apps/NFTOrig.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../../safeguard/Ownable.sol";
+
+contract OrigNFT is ERC721URIStorage, Ownable {
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+    }
+
+    function mint(address to, uint256 id, string memory uri) external onlyOwner {
+        _mint(to, id);
+        _setTokenURI(id, uri);
+    }
+}

c8ce54ad6b3cdd933e2ecf683a38949e2c8801e8
Junda Liu <junda@celer.network>
2022-04-09 11:53:49 -0700

    add deploy ts for mcn nft

---
+++ deploy/message/apps/004_nft_mcn.ts
@@ -0,0 +1,25 @@
+import * as dotenv from 'dotenv';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+dotenv.config();
+
+const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy('MCNNFT', {
+    from: deployer,
+    log: true,
+    args: [
+      process.env.NFT_NAME,
+      process.env.NFT_SYM,
+      process.env.NFT_BRIDGE_ADDR
+    ]
+  });
+};
+
+deployFunc.tags = ['MCNNFT'];
+deployFunc.dependencies = [];
+export default deployFunc;

efd32c5db47904674821276eac37965ad8a20484
ClaudeZsb <baobaobao369@163.com>
2022-08-18 11:56:24 +0800

    add status:RefundInitiated

--- contracts/message/apps/RFQ.sol
+++ contracts/message/apps/RFQ.sol
@@ -36,13 +36,14 @@ contract RFQ is MessageSenderApp, MessageReceiverApp, Pauser, ReentrancyGuard {
 
     enum QuoteStatus {
         Null,
-        DepositedNormal, // sender deposited non-native token               || location: src chain
-        DepositedNative, // sender deposited native token                   || location: src chain
-        ReleasedNormal, // released non-native token to liquidityProvider  || location: src chain
-        ReleasedNative, // released native token to liquidityProvider      || location: src chain
-        RefundedNormal, // refunded non-native token to refundTo/Sender    || location: src chain
-        RefundedNative, // refunded native token to refundTo/Sender        || location: src chain
-        ExecutedNormal, // transferred non-native token to receiver        || location: dst chain
+        DepositedNormal, // sender deposited non-native token             || location: src chain
+        DepositedNative, // sender deposited native token                 || location: src chain
+        ReleasedNormal, // released non-native token to liquidityProvider || location: src chain
+        ReleasedNative, // released native token to liquidityProvider     || location: src chain
+        RefundedNormal, // refunded non-native token to refundTo/Sender   || location: src chain
+        RefundedNative, // refunded native token to refundTo/Sender       || location: src chain
+        RefundInitiated, // refund initiated                              || location: dst chain
+        ExecutedNormal, // transferred non-native token to receiver       || location: dst chain
         ExecutedNative // transferred native token to reciever            || location: dst chain
     }
 
@@ -172,13 +173,14 @@ contract RFQ is MessageSenderApp, MessageReceiverApp, Pauser, ReentrancyGuard {
         uint256[] calldata _powers
     ) external payable nonReentrant whenNotPaused {
         require(_quote.deadline < block.timestamp, "Rfq: not past release deadline");
+        address _receiver = remoteRfqContracts[_quote.srcChainId];
+        require(_receiver != address(0), "Rfq: no rfq contract on src chain");
         bytes32 quoteHash = getQuoteHash(_quote);
         receiveMsgAndCheckHash(_message, _route, _sigs, _signers, _powers, quoteHash);
         require(quotes[quoteHash] == QuoteStatus.Null, "Rfq: quote already executed");
         delete unconsumedMsg[quoteHash];
 
-        address _receiver = remoteRfqContracts[_quote.srcChainId];
-        require(_receiver != address(0), "Rfq: no rfq contract on src chain");
+        quotes[quoteHash] == QuoteStatus.RefundInitiated;
         bytes memory message = abi.encode(quoteHash);
         sendMessage(_receiver, _quote.srcChainId, message, msg.value);
         emit RefundInitiated(quoteHash);

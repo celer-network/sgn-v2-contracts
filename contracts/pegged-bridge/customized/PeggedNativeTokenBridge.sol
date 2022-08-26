9baf13c25f4a2e004d45e287f984f364c282fd38
Xiaozhou Li <xli@celer.network>
2022-08-25 22:28:31 -0700

    remove wrap token

--- contracts/pegged-bridge/customized/PeggedNativeTokenBridge.sol
+++ contracts/pegged-bridge/customized/PeggedNativeTokenBridge.sol
@@ -4,12 +4,13 @@ pragma solidity 0.8.9;
 
 import "../PeggedTokenBridgeV2.sol";
 
-interface INativeWrap {
+interface INativeVault {
     function burn() external payable;
 }
 
 contract PeggedNativeTokenBridge is PeggedTokenBridgeV2 {
-    address public nativeWrap;
+    // native vault address is treated as the pegged natvie token address
+    address public nativeVault;
 
     constructor(ISigsVerifier _sigsVerifier) PeggedTokenBridgeV2(_sigsVerifier) {}
 
@@ -18,13 +19,13 @@ contract PeggedNativeTokenBridge is PeggedTokenBridgeV2 {
         address _toAccount,
         uint64 _nonce
     ) external payable whenNotPaused returns (bytes32) {
-        require(msg.value > 0, "zero amount");
-        bytes32 burnId = _burn(nativeWrap, msg.value, _toChainId, _toAccount, _nonce);
-        INativeWrap(nativeWrap).burn{value: msg.value}();
+        require(msg.value > 0, "zero msg value");
+        bytes32 burnId = _burn(nativeVault, msg.value, _toChainId, _toAccount, _nonce);
+        INativeVault(nativeVault).burn{value: msg.value}();
         return burnId;
     }
 
-    function setNativeWrap(address _natvieWrap) external onlyOwner {
-        nativeWrap = _natvieWrap;
+    function setNativeVault(address _natvieVault) external onlyOwner {
+        nativeVault = _natvieVault;
     }
 }

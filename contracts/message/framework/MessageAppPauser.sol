02b0d9dcf78699e319a9ec1ca6a39f266955923e
Xiaozhou Li <xli@celer.network>
2023-01-23 16:49:21 -0800

    add message app pauser

---
+++ contracts/message/framework/MessageAppPauser.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity >=0.8.0;
+
+import "../libraries/MsgDataTypes.sol";
+import "../../safeguard/Pauser.sol";
+
+abstract contract MessageAppPauser is Pauser {
+    /**
+     * @dev Modifier to make the message execution function callable only when 
+     * the contract is not paused. 
+     *
+     * Added the ABORT_PREFIX ("MSG::ABORT:") in front of the revert message to
+     * work with the Celer IM MessageBus contract.
+     */
+    modifier whenNotMsgPaused() {
+        require(!paused(), string.concat(MsgDataTypes.ABORT_PREFIX, "Pausable: paused"));
+        _;
+    }
+}

d2e8cd79567f952e0bc08a8b21634ec875392af8
Elliott He <elliott.he@outlook.com>
2023-05-10 17:40:18 +0800

    refine deploy

--- deploy/pegged-bridge/000_mint_swap_canonical_token.ts
+++ deploy/pegged-bridge/000_mint_swap_canonical_token.ts
@@ -8,14 +8,14 @@ import { Deployer } from '@matterlabs/hardhat-zksync-deploy';
 dotenv.config();
 
 // how to use, from the root directory:
-// yarn hardhat deploy-zksync --network zkSync --script deploy/liquidity-bridge/000_bridge.ts
-const deployFunc = async (hre: HardhatRuntimeEnvironment) => {
+// yarn hardhat deploy-zksync --network zkSync --script deploy/pegged-bridge/000_mint_swap_canonical_token.ts
+export default async function(hre: HardhatRuntimeEnvironment) {
   // Initialize the wallet.
   const wallet = new Wallet(process.env.ZK_SYNC_TEST_PRIVATE_KEY as string);
 
   // Create deployer object and load the artifact of the contract we want to deploy.
   const deployer = new Deployer(hre, wallet);
-  const artifact = await deployer.loadArtifact('Bridge');
+  const artifact = await deployer.loadArtifact('MintSwapCanonicalToken');
 
   // Estimate contract deployment fee
   // const deploymentFee = await deployer.estimateDeployFee(artifact, []);
@@ -33,11 +33,22 @@ const deployFunc = async (hre: HardhatRuntimeEnvironment) => {
   //console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
 
   // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
-  const bridgeContract = await deployer.deploy(artifact, []);
+  const args = [
+    process.env.MINT_SWAP_CANONICAL_TOKEN_NAME as string,
+    process.env.MINT_SWAP_CANONICAL_TOKEN_SYMBOL as string,
+    process.env.MINT_SWAP_CANONICAL_TOKEN_DECIMALS,
+  ];
+  const contract = await deployer.deploy(artifact, args);
 
   // Show the contract info.
-  const contractAddress = bridgeContract.address;
+  const contractAddress = contract.address;
   console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
-};
 
-export default deployFunc;
+  const verificationId = await hre.run("verify:verify", {
+    address: contractAddress,
+    contract: "contracts/pegged-bridge/tokens/MintSwapCanonicalToken.sol:MintSwapCanonicalToken",
+    constructorArguments: args
+  });
+
+  console.log(`verification id is ${verificationId}`);
+};

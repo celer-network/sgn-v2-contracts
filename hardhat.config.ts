ec4f918339e78fce278239b95e67315440e1a1f6
Michael Zhou <zhoumotongxue008@gmail.com>
2023-08-13 20:39:34 -0700

    Add Base to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -234,6 +234,9 @@ const antimatterB2PrivateKey = process.env.ANTIMATTER_B2_PRIVATE_KEY || DEFAULT_
 const lineaEndpoint = process.env.LINEA_ENDPOINT || DEFAULT_ENDPOINT;
 const lineaPrivateKey = process.env.LINEA_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
 
+const baseEndpoint = process.env.BASE_ENDPOINT || DEFAULT_ENDPOINT;
+const basePrivateKey = process.env.BASE_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
+
 const telosEndpoint = process.env.TELOS_ENDPOINT || DEFAULT_ENDPOINT;
 const telosPrivateKey = process.env.TELOS_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
 
@@ -241,13 +244,13 @@ const telosPrivateKey = process.env.TELOS_PRIVATE_KEY || DEFAULT_PRIVATE_KEY;
 function getNetworkConfig(url: string, kmsKeyId: string, privateKey: string, gasPrice?: number): NetworkUserConfig {
   const network: NetworkUserConfig = !kmsKeyId
     ? {
-      url: url,
-      accounts: [`0x${privateKey}`]
-    }
+        url: url,
+        accounts: [`0x${privateKey}`]
+      }
     : {
-      url: url,
-      kmsKeyId: kmsKeyId
-    };
+        url: url,
+        kmsKeyId: kmsKeyId
+      };
   if (gasPrice) {
     network.gasPrice = gasPrice;
   }
@@ -408,6 +411,7 @@ const config: HardhatUserConfig = {
     polygonZkevm: getNetworkConfig(polygonZkevmEndpoint, kmsKeyId, polygonZkevmPrivateKey),
     antimatterB2: getNetworkConfig(antimatterB2Endpoint, kmsKeyId, antimatterB2PrivateKey),
     linea: getNetworkConfig(lineaEndpoint, kmsKeyId, lineaPrivateKey),
+    base: getNetworkConfig(baseEndpoint, kmsKeyId, basePrivateKey),
     telos: getNetworkConfig(telosEndpoint, kmsKeyId, telosPrivateKey)
   },
   namedAccounts: {
@@ -460,7 +464,8 @@ const config: HardhatUserConfig = {
       moonbeam: process.env.MOONBEAM_MOONSCAN_API_KEY || '',
       heco: process.env.HECOSCAN_API_KEY || '',
       arbitrumNova: process.env.ARBISCAN_NOVA_API_KEY || '',
-      linea: process.env.LINEA_API_KEY || ''
+      linea: process.env.LINEA_API_KEY || '',
+      base: process.env.BASE_API_KEY || ''
     },
     customChains: [
       {
@@ -477,6 +482,14 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: process.env.LINEA_API_ENDPOINT || '',
           browserURL: process.env.LINEA_EXPLORER || ''
+        }
+      },
+      {
+        network: 'base',
+        chainId: 8453,
+        urls: {
+          apiURL: process.env.BASE_API_ENDPOINT || '',
+          browserURL: process.env.BASE_EXPLORER || ''
         }
       }
     ]

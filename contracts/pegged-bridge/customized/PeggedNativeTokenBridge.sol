6c542b7d01cc9e5f4f70a011d1b25b4b00f0894e
Xiaozhou Li <xl@cs.princeton.edu>
2026-05-20 00:31:40 -0700

    improve PeggedNativeTokenBridge

--- contracts/pegged-bridge/customized/PeggedNativeTokenBridge.sol
+++ contracts/pegged-bridge/customized/PeggedNativeTokenBridge.sol
@@ -9,7 +9,7 @@ interface INativeVault {
 }
 
 contract PeggedNativeTokenBridge is PeggedTokenBridgeV2 {
-    // native vault address is treated as the pegged natvie token address
+    // native vault address is treated as the pegged native token address
     address public nativeVault;
 
     constructor(ISigsVerifier _sigsVerifier) PeggedTokenBridgeV2(_sigsVerifier) {}
@@ -20,12 +20,13 @@ contract PeggedNativeTokenBridge is PeggedTokenBridgeV2 {
         uint64 _nonce
     ) external payable whenNotPaused returns (bytes32) {
         require(msg.value > 0, "zero msg value");
+        require(nativeVault != address(0), "native vault not set");
         bytes32 burnId = _burn(nativeVault, msg.value, _toChainId, _toAccount, _nonce);
         INativeVault(nativeVault).burn{value: msg.value}();
         return burnId;
     }
 
-    function setNativeVault(address _natvieVault) external onlyOwner {
-        nativeVault = _natvieVault;
+    function setNativeVault(address _nativeVault) external onlyOwner {
+        nativeVault = _nativeVault;
     }
 }
